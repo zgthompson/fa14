bbdb395e84b44699a607b5a1865d36f1b7d6c6dd
Zachary Thompson <zgthompson@gmail.com>
2014-09-08 11:08:35 -0700

    hw2 now reads in a file

--- programming-languages/hw2/thompsonH02.cpp
+++ programming-languages/hw2/thompsonH02.cpp
@@ -3,16 +3,32 @@
 #include "fraction.h"
 
 #include <iostream>     // cin, cout
+#include <fstream>
 #include <vector>       // vector
 #include <algorithm>    // sort
 
 
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    // check for proper number of command line arguments
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " FILENAME" << std::endl;
+        return 1;
+    }
 
     // optimizations that make cin and cout faster
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
+
+    // open fraction file
+    std::ifstream fractionFile(argv[1]);
+
+    // exit if file was unable to open
+    if (!fractionFile.is_open()) {
+        std::cerr << "Unable to open file" << std::endl;
+        return 1;
+    }
 
     // preallocate plenty of space
     std::vector<Fraction> fractions;
@@ -21,10 +37,12 @@ int main() {
     // create all the fractions
     Fraction fractionSum;
     Fraction fraction;
-    while (std::cin >> fraction) {
+    while (fractionFile >> fraction) {
         fractions.push_back(fraction); 
         fractionSum += fraction;
     }
+    
+    fractionFile.close();
 
     std::cout << "The sum of the fractions is: " << fractionSum << "\n";
 
@@ -35,4 +53,6 @@ int main() {
     for (int i = 0; i < fractions.size(); ++i) {
         std::cout << fractions[i] << "\n";
     }
+
+    return 0;
 }
