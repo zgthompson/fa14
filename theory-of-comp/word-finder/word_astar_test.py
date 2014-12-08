a139d952c3d58a2186b3e2b6acbc7e66bf391dc2
Zachary Thompson <zgthompson@gmail.com>
2014-12-08 07:17:53 -0800

    pre final commit

---
+++ theory-of-comp/word-finder/word_astar_test.py
@@ -0,0 +1,12 @@
+from word_astar import *
+import sys
+
+def main():
+    if len(sys.argv) != 3:
+        print "%s WORD1 WORD2" % sys.argv[0]
+        return
+
+    a = WordAStar(sys.argv[1], sys.argv[2])
+    a.search()
+
+main()
